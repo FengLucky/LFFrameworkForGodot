4333d57e6adb0f6a7a06875e3fa8463fe83bae4d
luckyfeng <1101459726@qq.com>
2025-07-07 01:16:51 +0800

    修改 xsd 规则，强制 bean 命名以Bean结尾、table 命名以Table结尾，修改相应 struct 定义

--- Runtime/Config/LFExternalTypeUtil.cs
+++ Runtime/Config/LFExternalTypeUtil.cs
@@ -3,16 +3,16 @@ using Godot;
 namespace Config;
 public static class LFExternalTypeUtil
 {
-    public static Vector2 NewVector2(CfgVector2 vector)
+    public static Vector2 NewVector2(Vector2Bean vector)
     {
         return new Vector2(vector.X, vector.Y);
     }
-    public static Vector3 NewVector3(CfgVector3 vector)
+    public static Vector3 NewVector3(Vector3Bean vector)
     {
         return new Vector3(vector.X, vector.Y, vector.Z);
     }
 
-    public static Color NewColor(CfgColor color)
+    public static Color NewColor(ColorBean color)
     {
         return new Color(color.R/255f, color.G/255f, color.B/255f, color.A/255f);
     }
